e31e5fdc82a7472635ce1e61cee0bfc24d93c753
ving9 <rhymester139@gmail.com>
2023-12-14 20:10:32 +0900

    df

--- Cplusbegin/main.cpp
+++ Cplusbegin/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int main() {
+int main(void) {
 
-	int var;
+	int i = 0;
+	// 정수형 : char(1), short(2), int(4), long(4), long long(8)
+	// 실수형 : float(4), double(8)
 
-	std::cout << "Hello World!" << std::endl;
-	// 테스트용 글쓰기 입니다
 
 	return 0;
 }
